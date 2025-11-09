428d3718c840bcb7207c14b33187edbf4f786ae1
Vincent <31067272+AmoyCherry@users.noreply.github.com>
2025-11-09 17:35:43 -0500

    rollback

--- syzLLM_server.py
+++ syzLLM_server.py
@@ -123,8 +123,7 @@ def fill_mask(sequence,
     input_ids = input_ids_tensor.data['input_ids']
     mask_token_index = torch.where(input_ids == 11243)[1]
     mask_token_logits = mask_model(input_ids).logits[0, mask_token_index, :]
-    #top_tokens = sample(mask_token_logits, sampling_method, temperature, top_k, top_p, beam_width, diversity_penalty)
-    top_tokens = sample_with_top_p(mask_token_logits, top_p)
+    top_tokens = sample(mask_token_logits, sampling_method, temperature, top_k, top_p, beam_width, diversity_penalty)
 
     syscalls = []
     exclude_calls = ["image", "[SEP]", "[CLS]", "[UNK]", "[MASK]", "[PAD]"]
